dac713b093dbd3cb2fa4bf7bad160d8107a395f2
Marcoo09 <marcofiorito1@hotmail.com>
2018-07-31 07:37:16 -0300

    Add files via upload

---
+++ Practico2/EjerciciosExtraStrings/src/ejerciciosextrastrings/EjerciciosExtraStrings.java
@@ -0,0 +1,33 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosextrastrings;
+
+import java.util.*;
+/**
+ *
+ * @author Usuario
+ */
+public class EjerciciosExtraStrings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner input = new Scanner(System.in);
+        
+        String entry;
+        
+        entry = input.nextLine();
+        
+        System.out.println(entry.replaceAll(" ", ""));
+        
+    }
+    
+}
